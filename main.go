b193ead18c50e0c35516c27822f6ee5d44867654
movsb <anhbk@qq.com>
2018-08-29 20:13:07 +0800

    feat: commandline arguments

--- main.go
+++ main.go
@@ -2,31 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
-func main() {
-	input, err := os.Open("main.tao")
-	if err != nil {
-		panic(err)
-	}
-	defer input.Close()
-
+func exec(input io.Reader) {
 	tokenizer := NewTokenizer(input)
-
 	parser := NewParser(tokenizer)
-
 	program, err := parser.Parse()
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
-
 	ret, err := program.Execute()
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
+	_ = ret
+}
 
-	fmt.Println("main exit:", ret)
+func main() {
+	if len(os.Args) == 1 {
+		exec(os.Stdin)
+	} else {
+		file, err := os.Open(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		exec(file)
+	}
 }
