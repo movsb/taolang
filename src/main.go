ef572849cdf4e552afd743525b1ad6430e2d84e4
movsb <anhbk@qq.com>
2018-09-11 20:57:40 +0800

    feat: demo setTimeout

--- src/main.go
+++ src/main.go
@@ -3,10 +3,30 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
+	"time"
 )
 
-func exec(input io.Reader) {
+type MessageType uint
+
+const (
+	_ MessageType = iota
+	mtExit
+	mtExec
+	mtTimer
+)
+
+type Message struct {
+	mt   MessageType
+	data interface{}
+}
+
+var queue = make(chan Message, 1)
+var exit = make(chan int)
+
+func exec(input io.ReadCloser) {
+	defer input.Close()
 	tokenizer := NewTokenizer(input)
 	parser := NewParser(tokenizer)
 	program, err := parser.Parse()
@@ -23,14 +43,43 @@ func exec(input io.Reader) {
 }
 
 func main() {
+	go func() {
+		for {
+			select {
+			case msg := <-queue:
+				switch msg.mt {
+				case mtExit:
+					exit <- 0
+				case mtExec:
+					exec(msg.data.(io.ReadCloser))
+				case mtTimer:
+					t := msg.data.(*Timer)
+					c := &CallExpression{
+						Callable: t.callback,
+						Args:     &Arguments{},
+					}
+					c.Evaluate(t.ctx)
+				}
+			case <-time.After(time.Hour):
+			}
+		}
+	}()
+
 	if len(os.Args) == 1 {
-		exec(os.Stdin)
+		queue <- Message{
+			mt:   mtExec,
+			data: ioutil.NopCloser(os.Stdin),
+		}
 	} else {
 		file, err := os.Open(os.Args[1])
 		if err != nil {
 			panic(err)
 		}
-		defer file.Close()
-		exec(file)
+		queue <- Message{
+			mt:   mtExec,
+			data: file,
+		}
 	}
+
+	<-exit
 }
